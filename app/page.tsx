e17e6b7a226a4bf7162dca101b61362d90b0d3ed
PedrOliveiraM <pedropucmont@gmail.com>
2024-10-30 10:41:09 -0300

    chore: update setup prisma and shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-
 export default function Home() {
   return (
-    <button className="flex-col flex text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">...</button>
-  );
+    <button className="flex flex-col bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+      ...
+    </button>
+  )
 }
